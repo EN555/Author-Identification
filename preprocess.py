c96a65e1e21fb5e0344cde2ebd6645b6ded2d0d9
Yosef Danan <yosefdanan1234@gmail.com>
2021-11-12 12:11:34 +0200

    cosmetics

--- preprocess.py
+++ preprocess.py
@@ -9,43 +9,13 @@ import sys
 import vaex
 import nltk
 
-def get_data_to_df() -> pd.DataFrame:
-    """
-    will save the whole data in csv format with
-    ["id","text","author"] columns
-    :return: result df
-    """
-    df = pd.DataFrame()
-    data_path = "./books1/epubtxt"
-    for i,filename in enumerate(next(os.walk(data_path))[2]):
-        if i > 100:
-            break
-        with open(os.path.join(data_path,filename),"r",encoding="utf8") as file:
-            text = ""
-            author = ""
-            title = ""
-            for line in file.readlines():
-                if line == "":
-                    continue
-                print(line.replace("\n"," "))
-                if "copyright" in line.lower():
-                    pass
-                    # print(line)
-                    # line = line.lower()
-                    # line = line.split(" ")
-                    # author = " ".join(line[line.index("by"):])
-                elif author != "":
-                    text += line
-            print(author,text,title)
-    # df.to_csv("data.csv")
-    return df
-
 
 def get_data() -> pd.DataFrame:
     # columns = ["Text", "title", "author", "publish"]
     data_path = "./books1/epubtxt"
     txt_files = pd.Series(next(os.walk(data_path))[-1])
-    clean_txt_files = txt_files.swifter.apply(lambda s: re.sub(r"(\d-)|(-\d)","",s).replace('.txt','').replace(".epub","")).values
+    clean_txt_files = txt_files.swifter.apply(
+        lambda s: re.sub(r"(\d-)|(-\d)", "", s).replace('.txt', '').replace(".epub", "")).values
     rows = []
     with open("url_list.json", "r") as url_file:
         for line in tqdm(url_file.readlines()):
@@ -59,7 +29,7 @@ def get_data() -> pd.DataFrame:
                     book_name = dict_row['epub']
                 else:
                     continue
-                book_name = book_name.replace(".txt","").replace(".epub","").split('/')[-1]
+                book_name = book_name.replace(".txt", "").replace(".epub", "").split('/')[-1]
                 if book_name not in clean_txt_files:
                     continue
                 filenames = txt_files[clean_txt_files == book_name]
@@ -71,21 +41,22 @@ def get_data() -> pd.DataFrame:
 
                 for filename in filenames:
                     curr_row = series_row.copy()
-                    with open(os.path.join(data_path,filename),encoding="utf8") as file:
+                    with open(os.path.join(data_path, filename), encoding="utf8") as file:
                         curr_row['Text'] = file.read()
                         rows.append(curr_row)
             except Exception as e:
                 print(dict_row)
                 print(e)
     df = pd.concat(rows, axis=1).transpose()
-    df.to_csv("data.csv",quoting=csv.QUOTE_ALL)
+    df.to_csv("data.csv", quoting=csv.QUOTE_ALL)
     return df
 
-"""
-replace author's names to numebrs 
-and drop 3 not important columns 
-"""
-def replace_name() ->pd.DataFrame:
+
+def replace_name() -> pd.DataFrame:
+    """
+    replace author's names to numebrs
+    and drop 3 not important columns
+    """
     data = pd.read_csv('data.csv')
     data.drop(["publish", "genres", "title", "Unnamed: 0"], axis=1, inplace=True)
     values = [i for i in range(0, data["author"].value_counts().sum())]
@@ -94,11 +65,13 @@ def replace_name() ->pd.DataFrame:
     data["author"] = data["author"].swifter.apply(lambda s: dic_name_id[s])
     return data
 
+
 def func(text):
     x = text.split(" ")
     n = 500
     res = [' '.join(x[i:i + n]) for i in range(0, (len(x) % n) * n, n)] + [' '.join(x[(len(x) % n) * n:len(x)])]
     return res
+
 
 def divide_book_to_chucnks(data):
     data["Text"] = data["Text"].swifter.apply(lambda x: func(x))
@@ -106,13 +79,17 @@ def divide_book_to_chucnks(data):
     data.to_csv("chuncks_data.csv")
     return data
 
+
 # average of sentence
-def average_sentence_size(data) ->pd.DataFrame:
-    data["sentence_len"] = data["Text"].swifter\
+def average_sentence_size(data) -> pd.DataFrame:
+    data["sentence_len"] = data["Text"].swifter \
         .apply(lambda text: pd.Series(nltk.sent_tokenize(text)).map(lambda sent: len(nltk.word_tokenize(sent))).mean())
+    return data
+
 
 # number of time uses comma
-def average_comma_uses(data) ->pd.DataFrame:
-    data["sentence_len"] = data["Text"].swifter\
-        .apply(lambda text: pd.Series(nltk.sent_tokenize(text)).map(lambda sent: pd.Series(sent).value_counts()[","]/len(nltk.word_tokenize(sent))).mean())
-
+def average_comma_uses(data) -> pd.DataFrame:
+    data["sentence_len"] = data["Text"].swifter \
+        .apply(lambda text: pd.Series(nltk.sent_tokenize(text)).map(
+            lambda sent: pd.Series(sent).value_counts()[","] / len(nltk.word_tokenize(sent))).mean())
+    return data
